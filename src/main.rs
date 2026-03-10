a714037244209cdc8aec5f67758c372362c12d0b
EduardoStarZ <pathver@gmail.com>
2026-03-10 15:55:07 -0300

    feat: println and exit functions

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,26 @@ use core::arch::asm;
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     let msg : &str = "Hello world!\n";
-    let buffer_ptr = msg.as_ptr();
-    let len : usize = msg.len();
+
+    println(msg);
+
+    exit();
+}
+
+pub fn exit() -> ! {
+    unsafe{ 
+        asm!(
+            "syscall",
+            in("rax") 60,
+            in("rdi") 0,
+            options(noreturn)
+        );
+    }
+}
+
+pub fn println(st: &str) {
+   let buffer_ptr = st.as_ptr();
+   let len : usize = st.len();
 
     unsafe {
         asm!(
@@ -19,14 +37,7 @@ pub extern "C" fn _start() -> ! {
             out("rcx") _,
             out("r11") _,
         );
-
-        asm!(
-            "syscall",
-            in("rax") 60,
-            in("rdi") 0,
-            options(noreturn)
-        );
-    }
+   } 
 }
 
 #[panic_handler]
